21ab7f7cab84f28ec99354c90d217af02f297a4b
Kfir <kfirfer@gmail.com>
2020-10-21 19:38:11 +0300

    wip

--- webcrawler/__init__.py
+++ webcrawler/__init__.py
@@ -5,7 +5,6 @@ os.environ["LOGGER_LEVEL"] = os.getenv("LOGGER_LEVEL", "debug")
 os.environ["LOG_CURL_REQUESTS"] = os.getenv("LOG_CURL_REQUESTS", "1")
 os.environ["MYSQL_LOG_QUERIES"] = os.getenv("MYSQL_LOG_QUERIES", "0")
 os.environ["ELASTICSEARCH_MONITOR_TAGS"] = os.getenv("ELASTICSEARCH_MONITOR_TAGS", "component:webcrawler")
-os.environ["HEADLESS_BROWSER"] = os.getenv("HEADLESS_BROWSER", "0")
 
 os.environ["MYSQL_USER"] = os.getenv("MYSQL_USER", "root")
 os.environ["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD", "123123")
